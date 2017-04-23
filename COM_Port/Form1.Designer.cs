d2d748223897e467c28d3afa67e75061969a9e8b
thongleeuitce <thonglee.uit.ce@gmail.com>
2017-04-23 22:31:32 +0700

    Added: save data and export to Text file (.txt)

--- COM_Port/Form1.Designer.cs
+++ COM_Port/Form1.Designer.cs
@@ -39,13 +39,15 @@
             this.lb_status = new System.Windows.Forms.Label();
             this.comboBox_BaudRate = new System.Windows.Forms.ComboBox();
             this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btn_save = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btn_open
             // 
-            this.btn_open.Location = new System.Drawing.Point(453, 314);
+            this.btn_open.Location = new System.Drawing.Point(380, 322);
             this.btn_open.Name = "btn_open";
-            this.btn_open.Size = new System.Drawing.Size(75, 23);
+            this.btn_open.Size = new System.Drawing.Size(85, 23);
             this.btn_open.TabIndex = 0;
             this.btn_open.Text = "Open";
             this.btn_open.UseVisualStyleBackColor = true;
@@ -54,7 +56,7 @@
             // comboBox_selectCOM
             // 
             this.comboBox_selectCOM.FormattingEnabled = true;
-            this.comboBox_selectCOM.Location = new System.Drawing.Point(431, 65);
+            this.comboBox_selectCOM.Location = new System.Drawing.Point(428, 53);
             this.comboBox_selectCOM.Name = "comboBox_selectCOM";
             this.comboBox_selectCOM.Size = new System.Drawing.Size(121, 21);
             this.comboBox_selectCOM.TabIndex = 1;
@@ -62,7 +64,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(428, 49);
+            this.label1.Location = new System.Drawing.Point(428, 33);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(67, 13);
             this.label1.TabIndex = 2;
@@ -75,10 +77,10 @@
             // 
             // txtbx_receiverData
             // 
-            this.txtbx_receiverData.Location = new System.Drawing.Point(89, 33);
+            this.txtbx_receiverData.Location = new System.Drawing.Point(50, 33);
             this.txtbx_receiverData.Multiline = true;
             this.txtbx_receiverData.Name = "txtbx_receiverData";
-            this.txtbx_receiverData.Size = new System.Drawing.Size(281, 248);
+            this.txtbx_receiverData.Size = new System.Drawing.Size(320, 264);
             this.txtbx_receiverData.TabIndex = 3;
             // 
             // Com
@@ -89,44 +91,66 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Location = new System.Drawing.Point(47, 17);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.Size = new System.Drawing.Size(77, 13);
             this.label2.TabIndex = 4;
-            this.label2.Text = "label2";
+            this.label2.Text = "Received data";
             // 
             // lb_status
             // 
             this.lb_status.AutoSize = true;
             this.lb_status.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.lb_status.Location = new System.Drawing.Point(450, 287);
+            this.lb_status.Location = new System.Drawing.Point(89, 322);
             this.lb_status.Name = "lb_status";
-            this.lb_status.Size = new System.Drawing.Size(61, 13);
+            this.lb_status.Size = new System.Drawing.Size(73, 13);
             this.lb_status.TabIndex = 5;
-            this.lb_status.Text = "Disconnect";
+            this.lb_status.Text = "Disconnected";
             // 
             // comboBox_BaudRate
             // 
             this.comboBox_BaudRate.FormattingEnabled = true;
-            this.comboBox_BaudRate.Location = new System.Drawing.Point(431, 121);
+            this.comboBox_BaudRate.Location = new System.Drawing.Point(428, 109);
             this.comboBox_BaudRate.Name = "comboBox_BaudRate";
             this.comboBox_BaudRate.Size = new System.Drawing.Size(121, 21);
             this.comboBox_BaudRate.TabIndex = 6;
+            this.comboBox_BaudRate.TextChanged += new System.EventHandler(this.comboBox_BaudRate_TextChanged);
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(431, 102);
+            this.label3.Location = new System.Drawing.Point(428, 90);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(94, 13);
             this.label3.TabIndex = 7;
             this.label3.Text = "Select Baud Rate:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(48, 322);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Status:";
+            // 
+            // btn_save
+            // 
+            this.btn_save.Location = new System.Drawing.Point(480, 322);
+            this.btn_save.Name = "btn_save";
+            this.btn_save.Size = new System.Drawing.Size(83, 23);
+            this.btn_save.TabIndex = 9;
+            this.btn_save.Text = "Save";
+            this.btn_save.UseVisualStyleBackColor = true;
+            this.btn_save.Click += new System.EventHandler(this.btn_save_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(579, 366);
+            this.Controls.Add(this.btn_save);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.comboBox_BaudRate);
             this.Controls.Add(this.lb_status);
@@ -154,6 +178,8 @@
         private System.Windows.Forms.Label lb_status;
         private System.Windows.Forms.ComboBox comboBox_BaudRate;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btn_save;
     }
 }
 
--- COM_Port/Form1.cs
+++ COM_Port/Form1.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO.Ports;
+using System.IO;
 
 namespace COM_Port
 {
@@ -19,28 +20,31 @@ namespace COM_Port
             InitializeComponent();
             init_Baund();
         }
+
         private void init_Baund()
         {
             int[] Baud = new int[] { 600, 1200, 2400, 4800, 9600, 14400, 19200, 38400, 56000, 57600, 115200 };
             comboBox_BaudRate.Items.Clear();
             for (int i = 0; i < Baud.Length; i++)
                 comboBox_BaudRate.Items.Add(Baud[i]);
+            comboBox_BaudRate.Text = Convert.ToString(Baud[4]);
+            Com.BaudRate = Convert.ToInt16(comboBox_BaudRate.Text);
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            if (lb_status.Text.Equals("Disconnect"))
+            if (lb_status.Text.Equals("Disconnected"))
             {
                 Com.PortName = comboBox_selectCOM.Text;
                 Com.Open();
                 btn_open.Text = "Close";
-                lb_status.Text = "Connect";
+                lb_status.Text = "Connected";
             }
             else
             {
                 Com.Close();
                 btn_open.Text = "Open";
-                lb_status.Text = "Disconnect";               
+                lb_status.Text = "Disconnected";               
             }
         }
         private void timer1_Tick(object sender, EventArgs e)
@@ -75,6 +79,20 @@ namespace COM_Port
             {
                 txtbx_receiverData.Text += s;   
             }
+        }
+
+        private void comboBox_BaudRate_TextChanged(object sender, EventArgs e)
+        {
+            Com.BaudRate = Convert.ToInt16(comboBox_BaudRate.Text);
+        }
+
+        private void btn_save_Click(object sender, EventArgs e)
+        {
+            FileInfo data = new FileInfo(@"D:\data.txt");
+            StreamWriter data_wr = data.CreateText();
+            data_wr.WriteLine(txtbx_receiverData.Text);
+            data_wr.Close();
+            txtbx_receiverData.Clear();
         }
     }
 }
